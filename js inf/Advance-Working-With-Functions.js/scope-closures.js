75d600c4159b11aa404c2576241b211af03b6baa
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2022-12-22 16:40:13 +0500

    .

---
+++ js inf/Advance-Working-With-Functions.js/scope-closures.js
@@ -0,0 +1,18 @@
+
+
+{
+    var a='abuabbaana';
+    console.log(a);
+}
+console.log(a);
+
+
+//  var is function and Let And Const is block scope we cannot acces let and Const outside the block 
+
+// example 
+{
+    let name='abuabbaana';
+    console.log(name);
+}
+console.log(name);
+
